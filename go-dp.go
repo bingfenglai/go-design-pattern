4cf97437757a11e31fc00b52369cc85e70530652
赖柄沣 <bingfengdev@aliyun.com>
2022-11-26 00:02:06 +0800

    feat：main函数

--- go-dp.go
+++ go-dp.go
@@ -1,7 +1,28 @@
-package go_dp
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
 
 func main() {
+	file, err := os.Open("./README.md")
+	defer file.Close()
 
+	if err != nil {
+		log.Default().Println(err.Error())
+	}
 
-
+	input := bufio.NewReader(file)
+	for {
+		readString, err := input.ReadString('\n')
+		fmt.Println(readString)
+		if err == io.EOF {
+			//fmt.Println(err)
+			return
+		}
+	}
 }
