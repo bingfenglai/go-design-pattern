a4b3b40e1e5fd1c3061318bc5b5096720dd6e0c6
赖柄沣 <bingfengdev@aliyun.com>
2022-12-14 22:47:45 +0800

    feat: 报表demo演示访问者模式

--- visitor/report.go
+++ visitor/report.go
@@ -11,8 +11,8 @@ func NewReport() *Report {
 		NewProgrammer("光脚大汉"),
 		NewProgrammer("时光殿主"),
 		NewProgrammer("南宫碗"),
-		NewManager("紫菱仙子"),
 
+		NewManager("紫菱仙子"),
 		NewManager("陈师姐"),
 	}
 	return &Report{employees: employees[:]}
